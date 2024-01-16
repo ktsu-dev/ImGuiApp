952233272bfdf08c5d235c6428dbefec4968a0f6
matt-edmondson <matthew.edmondson@gmail.com>
2024-01-16 17:41:04 +1100

    Remove DividerContainers and zones and Extensions which have been moved to their own respective libraries

--- ImGuiApp/Extensions.cs
+++
@@ -1,7 +0,0 @@
-namespace ktsu.io.ImGuiApp;
-
-public static class Extensions
-{
-	//from https://thomaslevesque.com/2019/11/18/using-foreach-with-index-in-c/
-	public static IEnumerable<(T item, int index)> WithIndex<T>(this IEnumerable<T> source) => source.Select((item, index) => (item, index));
-}
