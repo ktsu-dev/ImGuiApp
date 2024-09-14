5daa7c64b5202fecd7e4f649efd947c4b52c5cad
Matt Edmondson <matthew.edmondson@gmail.com>
2024-09-14 18:28:27 +1000

    Migrate ktsu.io to ktsu namespace

--- ImGuiAppDemo/Properties/Resources.Designer.cs
+++ ImGuiAppDemo/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ktsu.io.ImGuiApp.Demo.Properties {
+namespace ktsu.ImGuiApp.Demo.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace ktsu.io.ImGuiApp.Demo.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ktsu.io.ImGuiApp.Demo.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ktsu.ImGuiApp.Demo.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
