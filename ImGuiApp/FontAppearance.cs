2ad206ba27a5017d3b09b3dd08ccbe9519a3dd6a
Matt Edmondson <matthew.edmondson@gmail.com>
2025-03-12 22:03:18 +1100

    Add constructors and documentation to FontAppearance class
    Updated the `FontAppearance` class in `FontAppearance.cs` to include three new constructors for flexible initialization:
    - One constructor accepts a font name and size in points.
    - Another accepts only a size in points, using a default font name.
    - The last one accepts only a font name, using a default font size.
    
    XML documentation comments have been added to clarify the purpose and parameters of each constructor.

--- ImGuiApp/FontAppearance.cs
+++ ImGuiApp/FontAppearance.cs
@@ -33,4 +33,32 @@ public class FontAppearance : ScopedAction
 
 		OnClose = () => ImGuiApp.InvokeOnWindowThread(() => ImGui.PopFont());
 	}
+
+	/// <summary>
+	/// Initializes a new instance of the <see cref="FontAppearance"/> class.
+	/// Applies the specified font appearance to ImGui.
+	/// </summary>
+	/// <param name="name">The name of the font.</param>
+	/// <param name="sizePoints">The size of the font in points.</param>
+	public FontAppearance(string name, int sizePoints) : this(name, sizePoints, out _)
+	{
+	}
+
+	/// <summary>
+	/// Initializes a new instance of the <see cref="FontAppearance"/> class.
+	/// Applies the default font appearance to ImGui with the specified size in points.
+	/// </summary>
+	/// <param name="sizePoints">The size of the font in points.</param>
+	public FontAppearance(int sizePoints) : this(DefaultFontName, sizePoints, out _)
+	{
+	}
+
+	/// <summary>
+	/// Initializes a new instance of the <see cref="FontAppearance"/> class.
+	/// Applies the specified font appearance to ImGui with the default font size.
+	/// </summary>
+	/// <param name="name">The name of the font.</param>
+	public FontAppearance(string name) : this(name, DefaultFontPointSize, out _)
+	{
+	}
 }
