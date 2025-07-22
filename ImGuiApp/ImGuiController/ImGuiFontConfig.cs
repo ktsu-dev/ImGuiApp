b9df36a002cdb9650fdfed105450cf196968155f
Matt Edmondson <matthew.edmondson@gmail.com>
2025-07-22 13:24:43 +1000

    Refactor ImGuiFontConfig to enhance Unicode support by consolidating glyph range additions and improving code clarity. Removed redundant comments and streamlined the builder initialization process.

--- ImGuiApp/ImGuiController/ImGuiFontConfig.cs
+++ ImGuiApp/ImGuiController/ImGuiFontConfig.cs
@@ -54,80 +54,35 @@ public readonly struct ImGuiFontConfig : IEquatable<ImGuiFontConfig>
 	{
 		return new ImGuiFontConfig(fontPath, fontSize, io =>
 		{
-			var builder = new ImFontGlyphRangesBuilderPtr(ImGui.ImFontGlyphRangesBuilder());
-			
-			// Add default ranges (ASCII)
-			builder.AddRanges(io.Fonts.GetGlyphRangesDefault());
-			
-			// Add Latin Extended-A (U+0100-U+017F) - accented characters
-			builder.AddChar(0x0100, 0x017F);
-			
-			// Add Latin Extended-B (U+0180-U+024F) - more accented characters
-			builder.AddChar(0x0180, 0x024F);
-			
-			// Add General Punctuation (U+2000-U+206F) - various punctuation
-			builder.AddChar(0x2000, 0x206F);
-			
-			// Add Currency Symbols (U+20A0-U+20CF)
-			builder.AddChar(0x20A0, 0x20CF);
-			
-			// Add Mathematical Operators (U+2200-U+22FF)
-			builder.AddChar(0x2200, 0x22FF);
-			
-			// Add Geometric Shapes (U+25A0-U+25FF)
-			builder.AddChar(0x25A0, 0x25FF);
-			
-			// Add Miscellaneous Symbols (U+2600-U+26FF)
-			builder.AddChar(0x2600, 0x26FF);
-			
-			// Build the ranges
-			builder.BuildRanges(out ImVectorPtr ranges);
-			return (IntPtr)ranges.Data;
-		});
-	}
+			unsafe
+			{
+				ImFontGlyphRangesBuilderPtr builder = new(ImGui.ImFontGlyphRangesBuilder());
 
-	/// <summary>
-	/// Creates a font configuration with full Unicode support including CJK characters.
-	/// </summary>
-	/// <param name="fontPath">The path to the font file.</param>
-	/// <param name="fontSize">The size of the font.</param>
-	/// <returns>A font configuration with comprehensive Unicode glyph ranges.</returns>
-	public static ImGuiFontConfig WithFullUnicodeSupport(string fontPath, int fontSize)
-	{
-		return new ImGuiFontConfig(fontPath, fontSize, io =>
-		{
-			var builder = new ImFontGlyphRangesBuilderPtr(ImGui.ImFontGlyphRangesBuilder());
-			
-			// Add default ranges
-			builder.AddRanges(io.Fonts.GetGlyphRangesDefault());
-			
-			// Add Latin Extended
-			builder.AddRanges(io.Fonts.GetGlyphRangesLatinExt());
-			
-			// Add Cyrillic
-			builder.AddRanges(io.Fonts.GetGlyphRangesCyrillic());
-			
-			// Add CJK (Chinese, Japanese, Korean) - warning: this adds many glyphs
-			// builder.AddRanges(io.Fonts.GetGlyphRangesChineseFull());
-			// builder.AddRanges(io.Fonts.GetGlyphRangesJapanese());
-			// builder.AddRanges(io.Fonts.GetGlyphRangesKorean());
-			
-			// Add common Unicode blocks
-			builder.AddChar(0x2000, 0x206F); // General Punctuation
-			builder.AddChar(0x20A0, 0x20CF); // Currency Symbols
-			builder.AddChar(0x2100, 0x214F); // Letterlike Symbols
-			builder.AddChar(0x2190, 0x21FF); // Arrows
-			builder.AddChar(0x2200, 0x22FF); // Mathematical Operators
-			builder.AddChar(0x2300, 0x23FF); // Miscellaneous Technical
-			builder.AddChar(0x2400, 0x243F); // Control Pictures
-			builder.AddChar(0x2500, 0x257F); // Box Drawing
-			builder.AddChar(0x2580, 0x259F); // Block Elements
-			builder.AddChar(0x25A0, 0x25FF); // Geometric Shapes
-			builder.AddChar(0x2600, 0x26FF); // Miscellaneous Symbols
-			
-			// Build the ranges
-			builder.BuildRanges(out ImVectorPtr ranges);
-			return (IntPtr)ranges.Data;
+				// Add default ranges (ASCII)
+				builder.AddRanges(io.Fonts.GetGlyphRangesDefault());
+
+				// Add Latin Extended-A (U+0100-U+017F) - accented characters
+				builder.AddChar(0x0100, 0x017F);
+
+				// Add Latin Extended-B (U+0180-U+024F) - more accented characters
+				builder.AddChar(0x0180, 0x024F);
+
+				builder.AddChar(0x2000, 0x206F); // General Punctuation
+				builder.AddChar(0x20A0, 0x20CF); // Currency Symbols
+				builder.AddChar(0x2100, 0x214F); // Letterlike Symbols
+				builder.AddChar(0x2190, 0x21FF); // Arrows
+				builder.AddChar(0x2200, 0x22FF); // Mathematical Operators
+				builder.AddChar(0x2300, 0x23FF); // Miscellaneous Technical
+				builder.AddChar(0x2400, 0x243F); // Control Pictures
+				builder.AddChar(0x2500, 0x257F); // Box Drawing
+				builder.AddChar(0x2580, 0x259F); // Block Elements
+				builder.AddChar(0x25A0, 0x25FF); // Geometric Shapes
+				builder.AddChar(0x2600, 0x26FF); // Miscellaneous Symbols
+
+				// Build the ranges
+				builder.BuildRanges(out ImVectorPtr ranges);
+				return (IntPtr)ranges.Data;
+			}
 		});
 	}
 
@@ -142,23 +97,23 @@ public readonly struct ImGuiFontConfig : IEquatable<ImGuiFontConfig>
 	{
 		return new ImGuiFontConfig(fontPath, fontSize, io =>
 		{
-			var builder = new ImFontGlyphRangesBuilderPtr(ImGui.ImFontGlyphRangesBuilder());
-			
+			ImFontGlyphRangesBuilderPtr builder = new(ImGui.ImFontGlyphRangesBuilder());
+
 			// Add basic emoticons (U+1F600-U+1F64F)
 			builder.AddChar(0x1F600, 0x1F64F);
-			
+
 			// Add miscellaneous symbols and pictographs (U+1F300-U+1F5FF)
 			builder.AddChar(0x1F300, 0x1F5FF);
-			
+
 			// Add transport and map symbols (U+1F680-U+1F6FF)
 			builder.AddChar(0x1F680, 0x1F6FF);
-			
+
 			// Add supplemental symbols and pictographs (U+1F900-U+1F9FF)
 			builder.AddChar(0x1F900, 0x1F9FF);
-			
+
 			// Add symbols and pictographs extended-A (U+1FA70-U+1FAFF)
 			builder.AddChar(0x1FA70, 0x1FAFF);
-			
+
 			// Build the ranges
 			builder.BuildRanges(out ImVectorPtr ranges);
 			return (IntPtr)ranges.Data;
