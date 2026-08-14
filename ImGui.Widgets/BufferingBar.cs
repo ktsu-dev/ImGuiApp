13b8bd5729f7a6226109c31aa1ff54ee482ece8b
Matt Edmondson <matthew.edmondson@gmail.com>
2026-08-14 20:23:57 +1000

    Add Spinner and BufferingBar widgets backed by Hexa

---
+++ ImGui.Widgets/BufferingBar.cs
@@ -0,0 +1,23 @@
+// Copyright (c) 2023-2026 ktsu-dev contributors
+
+namespace ktsu.ImGui.Widgets;
+
+using System.Numerics;
+
+using ktsu.ImGui.Color;
+using ktsu.Semantics.Color;
+
+using HexaProgressBar = Hexa.NET.ImGui.Widgets.ImGuiProgressBar;
+
+public static partial class ImGuiWidgets
+{
+	/// <summary>
+	/// Draws a horizontal buffering bar filled from the left in proportion to <paramref name="value"/>.
+	/// </summary>
+	/// <param name="value">Fill fraction, clamped by the underlying implementation to the range 0 to 1.</param>
+	/// <param name="size">Size of the bar in pixels.</param>
+	/// <param name="background">Colour of the unfilled portion.</param>
+	/// <param name="foreground">Colour of the filled portion.</param>
+	public static void BufferingBar(float value, Vector2 size, Srgb background, Srgb foreground) =>
+		HexaProgressBar.ProgressBar(value, size, background.ToImGuiU32(), foreground.ToImGuiU32());
+}
---
+++ ImGui.Widgets/Spinner.cs
@@ -0,0 +1,20 @@
+// Copyright (c) 2023-2026 ktsu-dev contributors
+
+namespace ktsu.ImGui.Widgets;
+
+using ktsu.ImGui.Color;
+using ktsu.Semantics.Color;
+
+using HexaSpinner = Hexa.NET.ImGui.Widgets.ImGuiSpinner;
+
+public static partial class ImGuiWidgets
+{
+	/// <summary>
+	/// Draws an indeterminate loading spinner that animates from the ImGui frame time.
+	/// </summary>
+	/// <param name="radius">Radius of the spinner in pixels.</param>
+	/// <param name="thickness">Stroke thickness of the spinner arc in pixels.</param>
+	/// <param name="color">Colour of the spinner arc.</param>
+	public static void Spinner(float radius, float thickness, Srgb color) =>
+		HexaSpinner.Spinner(radius, thickness, color.ToImGuiU32());
+}
